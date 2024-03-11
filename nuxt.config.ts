6596f70bd30085e4e2bdd971aaa0de3635326839
Brandon Trecki <treckstar@gmail.com>
2024-03-11 03:18:25 -0400

    fix: add licens

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,31 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  extends: [process.env.NUXT_UI_PRO_PATH || '@nuxt/ui-pro'],
+  extends: [process.env.NUXT_UI_PRO_PATH || "@treckstar/ui-pro"],
   modules: [
-    '@nuxt/content',
-    '@nuxt/image',
-    '@nuxt/ui',
-    '@nuxt/fonts',
-    '@nuxthq/studio',
-    '@vueuse/nuxt',
-    'nuxt-og-image',
+    "@nuxt/content",
+    "@nuxt/image",
+    "@nuxt/ui",
+    "@nuxt/fonts",
+    "@nuxthq/studio",
+    "@vueuse/nuxt",
+    "nuxt-og-image",
   ],
   hooks: {
     // Define `@nuxt/ui` components as global to use them in `.md` (feel free to add those you need)
-    'components:extend': (components) => {
-      const globals = components.filter((c) => ['UButton'].includes(c.pascalName))
+    "components:extend": (components) => {
+      const globals = components.filter((c) =>
+        ["UButton"].includes(c.pascalName),
+      );
 
-      globals.forEach((c) => c.global = true)
-    }
+      globals.forEach((c) => (c.global = true));
+    },
   },
   ui: {
-    icons: ['heroicons', 'simple-icons']
+    icons: ["heroicons", "simple-icons"],
   },
   routeRules: {
-    '/api/search.json': { prerender: true },
-    '/docs': { redirect: '/docs/getting-started', prerender: false }
+    "/api/search.json": { prerender: true },
+    "/docs": { redirect: "/docs/getting-started", prerender: false },
   },
   devtools: {
-    enabled: true
-  }
-})
+    enabled: true,
+  },
+});
